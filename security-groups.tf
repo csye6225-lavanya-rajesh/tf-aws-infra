ba46683ba215bf7d30a2f6af7fa25ac3fb5f9109
Lavanya Rajesh <rajesh.l@northeastern.edu>
2025-02-19 01:21:02 -0500

    Removing hardcoded values from code

--- security-groups.tf
+++ security-groups.tf
@@ -2,20 +2,20 @@ resource "aws_security_group" "default" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = var.security_group_ingress_ports[0]
+    to_port     = var.security_group_ingress_ports[0]
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.security_group_ingress_cidr
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
+    from_port   = var.security_group_egress_ports[0]
+    to_port     = var.security_group_egress_ports[0]
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.security_group_egress_cidr
   }
 
   tags = {
-    Name = "tf-default-security-group"
+    Name = var.security_group_name
   }
 }
--- variables.tf
+++ variables.tf
@@ -39,3 +39,48 @@ variable "private_subnets" {
   type        = list(string)
   default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 }
+
+variable "route_table_tags" {
+  description = "Tags for the route tables"
+  type        = map(string)
+  default = {
+    public  = "tf-public-route-table"
+    private = "tf-private-route-table"
+  }
+}
+
+variable "default_cidr_block" {
+  description = "CIDR block for the default route"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "security_group_name" {
+  description = "Name for the security group"
+  type        = string
+  default     = "tf-default-security-group"
+}
+
+variable "security_group_ingress_ports" {
+  description = "List of ports allowed for ingress (e.g., 80 for HTTP)"
+  type        = list(number)
+  default     = [80]
+}
+
+variable "security_group_ingress_cidr" {
+  description = "List of CIDR blocks allowed for ingress traffic"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+variable "security_group_egress_ports" {
+  description = "List of ports allowed for egress (e.g., 0 for all ports)"
+  type        = list(number)
+  default     = [0]
+}
+
+variable "security_group_egress_cidr" {
+  description = "List of CIDR blocks allowed for egress traffic"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
