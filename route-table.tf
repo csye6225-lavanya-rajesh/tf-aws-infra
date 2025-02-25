df4302525158687f7f6781e7608a212aad2207cb
Lavanya Rajesh <rajesh.l@northeastern.edu>
2025-02-25 17:39:21 -0500

    Changes

--- route-table.tf
+++ route-table.tf
@@ -2,7 +2,7 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = varr.default_cidr_block # Reference the variable
+    cidr_block = var.default_cidr_block # Reference the variable
     gateway_id = aws_internet_gateway.gw.id
   }
 
