39cc885c241ae5ac3a0abcf4db9c489f26b172e4
Lavanya Rajesh <rajesh.l@northeastern.edu>
2025-02-27 02:28:51 -0500

    formatted ec3.tf file

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "app_server" {
   ami                    = var.ec2_ami_id
   instance_type          = var.ec2_instance_type
   key_name               = var.ec2_key_pair
-  subnet_id              = aws_subnet.public[0].id 
+  subnet_id              = aws_subnet.public[0].id
   vpc_security_group_ids = [aws_security_group.app_sg.id]
 
   root_block_device {
