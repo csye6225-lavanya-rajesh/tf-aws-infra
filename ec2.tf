51f347ea31d17f7acbef9c37eb28b852d12f5bd2
Lavanya Rajesh <rajesh.l@northeastern.edu>
2025-02-27 02:27:03 -0500

    Adding files for creating ec2 instance

---
+++ ec2.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "app_server" {
+  ami                    = var.ec2_ami_id
+  instance_type          = var.ec2_instance_type
+  key_name               = var.ec2_key_pair
+  subnet_id              = aws_subnet.public[0].id 
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+
+  root_block_device {
+    volume_size           = var.ec2_root_volume_size
+    volume_type           = var.ec2_root_volume_type
+    delete_on_termination = true
+  }
+
+  disable_api_termination = var.ec2_protect_termination
+
+  tags = {
+    Name = "App Instance-${var.env_name}"
+  }
+}
